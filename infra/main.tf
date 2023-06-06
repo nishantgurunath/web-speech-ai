15fbcd8c03ed53db9cff9fd510d5d6387b915a01
Nishant Gurunath <nishantgurunath@nishants-mbp.myfiosgateway.com>
2023-06-06 10:28:21 -0400

    added s3 backend for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.4.6"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "convbot-terraform-bucket"
+    region = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
