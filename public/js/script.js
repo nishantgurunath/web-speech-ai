e47e2ade7c43c9bcf8e0717e9fde0780bb5aee16
Nishant Gurunath <nishantgurunath@nishants-mbp.myfiosgateway.com>
2023-06-24 12:47:25 -0400

    Fixed speech utterances

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,21 @@ recognition.lang = lang;
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+const synth = window.speechSynthesis;
+var voices;
+var spanishVoices, germanVoices, italianVoices, dutchVoices;
+var hindiVoices, frenchVoices, portugueseVoices;
+speechSynthesis.onvoiceschanged = () => {
+  voices = synth.getVoices();
+  spanishVoices = voices.filter(voice => (voice.lang.startsWith('es') && voice.name.includes('Google')));
+  germanVoices = voices.filter(voice => (voice.lang.startsWith('de') && voice.name.includes('Google')));
+  italianVoices = voices.filter(voice => (voice.lang.startsWith('it') && voice.name.includes('Google')));
+  dutchVoices = voices.filter(voice => (voice.lang.startsWith('nl') && voice.name.includes('Google')));
+  hindiVoices = voices.filter(voice => (voice.lang.startsWith('hi') && voice.name.includes('Google')));
+  frenchVoices = voices.filter(voice => (voice.lang.startsWith('fr') && voice.name.includes('Google')));
+  portugueseVoices = voices.filter(voice => (voice.lang.startsWith('pt') && voice.name.includes('Google')));
+} 
+
 document.querySelector('button').addEventListener('click', () => {
   lang = document.getElementById('Language').value;
   recognition.lang = lang;
@@ -45,16 +60,38 @@ recognition.addEventListener('error', (e) => {
 });
 
 function synthVoice(text, lang) {
-  const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
   utterance.lang = lang;
+  console.log("language: " + utterance.lang)
+  if (lang == "es-ES") {
+    utterance.voice = spanishVoices[0]
+  }
+  else if (lang == "de-DE") {
+    utterance.voice = germanVoices[0]
+  }
+  else if (lang == "pt-PT") {
+    utterance.voice = portugueseVoices[0]
+  }
+  else if (lang == "it-IT") {
+    utterance.voice = italianVoices[0]
+  }
+  else if (lang == "hi-IN") {
+    utterance.voice = hindiVoices[0]
+  }
+  else if (lang == "fr-FR") {
+    utterance.voice = frenchVoices[0]
+  }
+  else if (lang == "nl-NL") {
+    utterance.voice = dutchVoices[0]
+  } 
+
   synth.speak(utterance);
 }
 
 socket.on('bot reply', function(replyText) {
   // alert("Lang: " + lang)
   synthVoice(replyText, lang);
-  if(replyText == '') replyText = '(No answer...)';
+  if(replyText == '') replyText = "I couldn't hear you";
   outputBot.textContent = replyText;
 });
